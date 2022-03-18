51143e3634e51c3d9c350be82a338433082b060a
XtereO <71135845+XtereO@users.noreply.github.com>
2022-03-18 08:54:19 +0300

    do scroll on change route

--- src/ui/Article/Article.tsx
+++ src/ui/Article/Article.tsx
@@ -1,18 +1,25 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import "./Article.css"
+import "./Article.css";
 import { getTheme } from "../../bll/selectors/presentationSelector";
 import { db } from "../../db/db";
 import { Slide } from "../Bricks/Slide/Slide";
-import {Slide as SlideType} from "../../types"
+import { Slide as SlideType } from "../../types";
 import { Navigation } from "../../consts";
 
 export const Article = memo(() => {
-  const theme = useSelector(getTheme)
-  const [content, setContent] = useState([])
-  useEffect(()=>{
+  const theme = useSelector(getTheme);
+  const [content, setContent] = useState([]);
+  useEffect(() => {
     //@ts-ignore
-    setContent(db[theme ?? Navigation.Home])
-  },[theme])
-  return <article id={"article"}>{content.map((c:SlideType)=><Slide {...c} key={`slide${c.slideId}`} />)}</article>;
+    setContent(db[theme ?? Navigation.Home]);
+    window.scrollTo({ top: 0 });
+  }, [theme]);
+  return (
+    <article id={"article"}>
+      {content.map((c: SlideType) => (
+        <Slide {...c} key={`slide${c.slideId}`} />
+      ))}
+    </article>
+  );
 });
